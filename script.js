88ed13afb1a6a01e1eb8eabdb364275744b4ba3d
EdS <Sousaa.dev@gmail.com.br>
2025-07-01 12:31:26 -0300

    second commit

--- script.js
+++ script.js
@@ -1,16 +1,68 @@
-const ul = document.getElementById("lista");
-const texto = document.getElementById("caixa");
-    
-function adicionarItem() {
-    if (texto.value != ""){
-        let novoItem = document.createElement("li");
+const ul = document.getElementById('lista');
+const texto = document.getElementById('caixa');
 
-        novoItem.innerHTML = `<img class="imagem" onclick="tarefaConcluida()" src="./assets/images/feito.png" alt="feito.png">${texto.value}<img class="imagem" src="./assets/images/removerX.png" alt="removerX.png">`;
-        ul.appendChild(novoItem);
+window.onload = carregarTarefas;
+
+function adicionarItem() {
+    if (texto.value !== "") {
+        const tarefa = {
+            texto: texto.value,
+            concluida: false
+        };
+
+        const tarefas = obterTarefas();
+        tarefas.push(tarefa);
+        salvarTarefas(tarefas);
+
+        renderizarTarefa(tarefa);
+        texto.value = "";
     } else {
-        alert("Tarefa está vazia, digite alguma tarefa")
+        alert('Tarefa está vazia, digite alguma tarefa');
     }
 }
 
-function tarefaConcluida() {
+function renderizarTarefa(tarefa, indice = null) {
+    const novoItem = document.createElement('li');
+    novoItem.classList.add('elemento');
+    if (tarefa.concluida) novoItem.classList.add('concluido');
+
+    novoItem.innerHTML = `
+        <img class="imagem feito" src="./assets/images/feito.png" alt="feito">
+        ${tarefa.texto}
+        <img class="imagem remover" src="./assets/images/removerX.png" alt="remover">
+    `;
+
+    const botaoFeito = novoItem.querySelector('.feito');
+    const botaoRemover = novoItem.querySelector('.remover');
+
+    botaoFeito.onclick = () => {
+        const tarefas = obterTarefas();
+        const index = Array.from(ul.children).indexOf(novoItem);
+        tarefas[index].concluida = !tarefas[index].concluida;
+        salvarTarefas(tarefas);
+        novoItem.classList.toggle('concluido');
+    };
+
+    botaoRemover.onclick = () => {
+        const tarefas = obterTarefas();
+        const index = Array.from(ul.children).indexOf(novoItem);
+        tarefas.splice(index, 1);
+        salvarTarefas(tarefas);
+        novoItem.remove();
+    };
+
+    ul.appendChild(novoItem);
+}
+
+function carregarTarefas() {
+    const tarefas = obterTarefas();
+    tarefas.forEach(tarefa => renderizarTarefa(tarefa));
+}
+
+function obterTarefas() {
+    return JSON.parse(localStorage.getItem('tarefas')) || [];
+}
+
+function salvarTarefas(tarefas) {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
 }
